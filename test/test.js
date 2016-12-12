502223daa4be42de36bce8075e0c8f2db3465ceb
Ivan Demidov <scrum@list.ru>
2016-12-12 16:10:32 +0300

    test(*): move config from package to test folder, fixed #4

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
-const readFile = require('fs').readFile;
-const posthtml = require('posthtml');
-const test = require('ava');
-const plugin = require('../src/index.js');
+import path from 'path';
+import {readFile} from 'fs';
+import posthtml from 'posthtml';
+import test from 'ava';
+import postLoadPlugins from '../src/index.js';
+
+process.chdir(path.resolve(process.cwd() + '/test'));
 
 const read = path => new Promise((resolve, reject) => {
 	readFile(path, 'utf8', (err, data) => {
@@ -13,26 +16,26 @@ const read = path => new Promise((resolve, reject) => {
 });
 
 test('Plugin return array', t => {
-	t.true(Array.isArray(plugin()));
+	t.true(Array.isArray(postLoadPlugins()));
 });
 
 test('Plugin reads custom json config from posthtml.json', async t => {
 	t.is(
-		(await read('test/expected/output-config-pkg.html')),
-		(await posthtml(plugin('test/fixtures/posthtml.json')).process(await read('test/fixtures/input.html'))).html
+		(await read('expected/output-config-pkg.html')),
+		(await posthtml(postLoadPlugins('fixtures/posthtml.json')).process(await read('fixtures/input.html'))).html
 	);
 });
 
 test('test witch posthtml-css-modules', async t => {
 	t.is(
-		(await read('test/expected/output-modules.html')),
-		(await posthtml(plugin('test/fixtures/modules.json', {'posthtml-css-modules': 'test/dist/css-modules.json', 'posthtml-each': {}})).process(await read('test/fixtures/input-modules.html'))).html
+		(await read('expected/output-modules.html')),
+		(await posthtml(postLoadPlugins('fixtures/modules.json', {'posthtml-css-modules': 'dist/css-modules.json', 'posthtml-each': {}})).process(await read('fixtures/input-modules.html'))).html
 	);
 });
 
 test('should throw not install plugin posthtml-css', async t => {
 	t.is(
-		(await read('test/expected/output-config-pkg.html')),
-		(await posthtml(plugin('test/fixtures/posthtml.json', {css: {}, postcss: {}})).process(await read('test/fixtures/input.html'))).html
+		(await read('expected/output-config-pkg.html')),
+		(await posthtml(postLoadPlugins('fixtures/posthtml.json', {css: {}, postcss: {}})).process(await read('fixtures/input.html'))).html
 	);
 });
