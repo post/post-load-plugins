2704091579897fde1dc038865a9505f98778bae4
Ivan Demidov <demidov@220-volt.ru>
2017-03-31 15:45:02 +0300

    style(post-processor): capitalized comments

--- src/post-processor.js
+++ src/post-processor.js
@@ -4,12 +4,12 @@ const findProcessorName = args => {
 	const ctx = args.find(ctx => Reflect.has(ctx, 'processor') && Reflect.has(ctx.processor, 'name'));
 	let name = ctx ? ctx.processor.name : undefined;
 
-	// detect postcss
+	// Detect postcss
 	if (name === undefined && args[0].type === 'root' && Reflect.has(args[1], 'processor')) {
 		name = 'postcss';
 	}
 
-	// detect reshape
+	// Detect reshape
 	if (name === undefined && Reflect.has(args[1], 'ReshapeError')) {
 		name = 'reshape';
 	}
